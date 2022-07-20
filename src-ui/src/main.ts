7d4fc917ac0ef104d3da5dce5bac579282cd20b4
YoungHa Kim <ytok1108@aol.com>
2022-07-20 21:14:43 +0900

    Click TypeScript

--- src-ui/src/main.ts
+++ src-ui/src/main.ts
@@ -1,3 +1,18 @@
+import { invoke } from "@tauri-apps/api";
+const $ = document.querySelector.bind(document);
+
 document.addEventListener("DOMContentLoaded", async function() {
-    // document.body.textContent = "Hello from main.ts";
-})
+    // get the elements
+    const helloEl = $("div.hello")! as HTMLElement;
+
+
+    // hello click
+    helloEl.addEventListener("pointerup", async function() {
+        const result = await invoke("hello_world") as string;
+        helloEl.textContent = result;
+        setTimeout(function() {
+            helloEl.textContent = "Click again";
+        }, 1000);
+    })
+});
+
